192c61d72d81dd8e341d4882ff7e848a29b8b092
metinsunbul@gmail.com <metinsunbul@gmail.com>
2022-09-26 23:35:10 +0300

    Discount Grpc implemented both as server as standalone and as client integrated in basket service

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,10 +13,12 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
         }
 
         [HttpGet("userName", Name ="GetBasket")]
@@ -30,6 +33,20 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            // : 1- Call the discount grpc service and 2- calculate the product's latest price
+            //Here the basket is client consuming discount grpc service
+            //Use connected services for creating client communicating server
+
+            //Consume Discount Grpc service from Basket Api microservice while adding card into shopping Card to calculate the final price of the product
+
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                
+                item.Price -= coupon.Amount;
+
+            }
+
             return Ok(await _repository.UpdateBasket(basket));
         }
 

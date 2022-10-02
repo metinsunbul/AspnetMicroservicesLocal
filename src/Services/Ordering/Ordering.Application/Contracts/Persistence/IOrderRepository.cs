7eaad080612534a54f25a7247b2ed1d002361c9a
metinsunbul@gmail.com <metinsunbul@gmail.com>
2022-10-02 14:34:27 +0300

    Repository implementation of abstract layer infrastructure completed

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+        
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Models/Email.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Models
+{
+    public class Email
+    {
+        public string To { get; set; }
+        public string Subjcet { get; set; }
+        public string Body { get; set; }
+
+    }
+}

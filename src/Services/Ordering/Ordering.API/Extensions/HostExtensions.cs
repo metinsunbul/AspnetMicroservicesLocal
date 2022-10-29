e3cc80ffc8ed5143dc9ff8db184498b809b4ff44
metinsunbul@gmail.com <metinsunbul@gmail.com>
2022-10-30 02:29:30 +0300

    Retry pattern with polly on sql migration

--- src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -3,17 +3,20 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Polly;
 using System;
 
 namespace Ordering.API.Extensions
 {
     public static class HostExtensions
     {
-        public static IHost MigrateDatabase<TContext>(this IHost host, 
-                                Action<TContext, IServiceProvider> seeder, 
-                                int? retry = 0) where TContext : DbContext
+        //public static IHost MigrateDatabase<TContext>(this IHost host, 
+        //                        Action<TContext, IServiceProvider> seeder, 
+        //                        int? retry = 0) where TContext : DbContext
+        public static IHost MigrateDatabase<TContext>(this IHost host,
+                               Action<TContext, IServiceProvider> seeder) where TContext : DbContext
         {
-            int retryForAvailability = retry.Value;
+            //int retryForAvailability = retry.Value;
 
             using (var scope = host.Services.CreateScope())
             {
@@ -25,8 +28,20 @@ namespace Ordering.API.Extensions
                 {
                     logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
 
+
+                    var retry = Policy.Handle<SqlException>()
+                        .WaitAndRetry(
+                           retryCount: 5,
+                           sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),//2,4,8,16,32 sec
+                            onRetry: (exception, retryCount, context) =>
+                            {
+                                logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                            });
+
+                    retry.Execute(() => IvokeSeeder(seeder, context, service));
+
                     //this will handle migration operation and also call the seeder method
-                    IvokeSeeder(seeder, context, service);
+                    //IvokeSeeder(seeder, context, service);
 
                     logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
 
@@ -34,13 +49,13 @@ namespace Ordering.API.Extensions
                 catch (SqlException ex)
                 {
                     logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
-
-                    if (retryForAvailability < 50)
-                    {
-                        retryForAvailability++;
-                        System.Threading.Thread.Sleep(2000);
-                        MigrateDatabase<TContext>(host, seeder, retryForAvailability);
-                    }
+                    //With using poly retry there is no need to manually apply retry pattern
+                    //if (retryForAvailability < 50)
+                    //{
+                    //    retryForAvailability++;
+                    //    System.Threading.Thread.Sleep(2000);
+                    //    MigrateDatabase<TContext>(host, seeder, retryForAvailability);
+                    //}
                 }
             }
 

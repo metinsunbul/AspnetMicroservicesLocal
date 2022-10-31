13215cf3f14d65ff0c5b14d9236c2e35195c8b1c
metinsunbul@gmail.com <metinsunbul@gmail.com>
2022-10-31 03:32:11 +0300

    Adding HealthCheck into Ordering.API miroservice and monitoring how it status changes from healthy to unhealthy when its dependency sql server and rabbitmq went down returning json instead of status(degraded or healthy, or unhealthy)

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,6 +1,8 @@
 using EventBus.Messages.Common;
+using HealthChecks.UI.Client;
 using MassTransit;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +11,7 @@ using Microsoft.OpenApi.Models;
 using Ordering.API.EventBusConsumer;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
 namespace Ordering.API
 {
@@ -33,11 +36,11 @@ namespace Ordering.API
             services.AddMassTransit(config => {
 
                 config.AddConsumer<BasketCheckoutConsumer>();
-
+                
                 config.UsingRabbitMq((ctx, cfg) =>
                 {
                     cfg.Host(Configuration["EventBusSettings:HostAddress"]);
-
+                    cfg.UseHealthCheck(ctx);//2nd subhealth check is rabbitmq connection, is obsolote now check it later :)
                     cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
                     {
                         c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
@@ -57,6 +60,11 @@ namespace Ordering.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
             });
+
+            //Add healthcheck
+            services.AddHealthChecks()
+                    .AddDbContextCheck<OrderContext>();//here Subhealth check is entityframework core, the 2nd subhealth check is rabbitmq connection
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -76,6 +84,13 @@ namespace Ordering.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                //endpoints.MapHealthChecks("/hc");
+                //Add HealthCheck middleware
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             });
         }
     }
